331ee1c28cb5889f982ecfd5d7b521389688083a
Bigeard <robin.bigeard@gmail.com>
2019-01-18 10:58:35 +0100

    socket

--- server.js
+++ server.js
@@ -1,32 +1,96 @@
-const express = require("express");
-const app = express();
+// var app = require("express")();
+// var http = require("http").Server(app);
+// var io = require("socket.io")(http);
+// var port = process.env.PORT || 3000;
+// // const MongoClient = require("mongodb").MongoClient;
+// // var db;
 
+// // MongoClient.connect(
+// //   "mongodb+srv://cronoses:nuggets@cluster0-lrtb2.mongodb.net/test?retryWrites=true",
+// //   { useNewUrlParser: true },
+// //   (err, client) => {
+// //     if (err) return console.log(err);
+// //     db = client.db("IOT"); // whatever your database name is
+
+//     app.get("/", function(req, res) {
+//       res.sendFile(__dirname + "/index.html");
+//     });
+
+//     io.on('connection', function(socket){
+//       console.log('a user connected');
+//       socket.on('disconnect', function(){
+//         console.log('user disconnected');
+//       });
+//     });
+
+//     // io.on("connection", function() {
+//     //   console.log("data");
+
+//     //   io.emit(
+//     //     "data",
+//     //     db
+//     //       .collection("data")
+//     //       .find()
+//     //       .sort({ _id: -1 })
+//     //       .limit(1)
+//     //       .toArray((err, result) => {
+//     //         if (err) return console.log(err);
+//     //         console.log(result);
+//     //       })
+//     //   );
+//     // });
+
+//     app.listen(port, () => {
+//       console.log("listening on http://localhost:" + port + "/");
+//     });
+// //   }
+// // );
+
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
+var data;
 const MongoClient = require("mongodb").MongoClient;
 var db;
 
-MongoClient.connect(
-  "mongodb+srv://cronoses:nuggets@cluster0-lrtb2.mongodb.net/test?retryWrites=true",
-  { useNewUrlParser: true },
-  (err, client) => {
-    if (err) return console.log(err);
-    db = client.db("IOT"); // whatever your database name is
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    app.get("/", (req, res) => {
-      let lastrecord = db
-        .collection("data")
+io.on("connection", function(socket) {
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  MongoClient.connect(
+    "mongodb+srv://cronoses:nuggets@cluster0-lrtb2.mongodb.net/test?retryWrites=true",
+    { useNewUrlParser: true },
+    (err, client) => {
+      if (err) return console.log(err);
+      db = client.db("IOT");
+      db.collection("data")
         .find()
-        .sort({ _id: 1 })
+        .sort({ _id: -1 })
         .limit(1)
         .toArray((err, result) => {
           if (err) return console.log(err);
           console.log(result);
-          // renders index.ejs
-          res.render("index.ejs", { data: result });
+          io.emit("data", result[0]);
         });
-    });
 
-    app.listen(3000, () => {
-      console.log("listening on localhost:3000");
-    });
-  }
-);
+      const stream = db.collection("data").watch();
+      stream.on("change", change => {
+        if (change.operationType === "insert") {
+          let result = [];
+          result.push(change.fullDocument);
+          console.log(result);
+          io.emit("data", result[0]);
+        }
+      });
+    }
+  );
+});
+
+http.listen(3000, function() {
+  console.log("listening on http://localhost:" + port + "/");
+});
