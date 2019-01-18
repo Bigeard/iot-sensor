9c5c29da1130f171232ff8b486446c8449a9116f
Bigeard <robin.bigeard@gmail.com>
2019-01-18 16:20:59 +0100

    add date and python file

---
+++ dht11.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import time
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("mongodb://cronoses:nuggets@cluster0-shard-00-00-lrtb2.mongodb.net:27017,cluster0-shard-00-01-lrtb2.mongodb.net:27017,cluster0-shard-00-02-lrtb2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
+db=client.IOT
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    localtime = time.localtime(time.time())
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    value = {
+        'temperature' : temperature,
+        'humidity' : humidity,
+        'date' : localtime
+    }
+
+    result=db.data.insert_one(value)
--- server.js
+++ server.js
@@ -1,58 +1,13 @@
-// var app = require("express")();
-// var http = require("http").Server(app);
-// var io = require("socket.io")(http);
-// var port = process.env.PORT || 3000;
-// // const MongoClient = require("mongodb").MongoClient;
-// // var db;
-
-// // MongoClient.connect(
-// //   "mongodb+srv://cronoses:nuggets@cluster0-lrtb2.mongodb.net/test?retryWrites=true",
-// //   { useNewUrlParser: true },
-// //   (err, client) => {
-// //     if (err) return console.log(err);
-// //     db = client.db("IOT"); // whatever your database name is
-
-//     app.get("/", function(req, res) {
-//       res.sendFile(__dirname + "/index.html");
-//     });
-
-//     io.on('connection', function(socket){
-//       console.log('a user connected');
-//       socket.on('disconnect', function(){
-//         console.log('user disconnected');
-//       });
-//     });
-
-//     // io.on("connection", function() {
-//     //   console.log("data");
-
-//     //   io.emit(
-//     //     "data",
-//     //     db
-//     //       .collection("data")
-//     //       .find()
-//     //       .sort({ _id: -1 })
-//     //       .limit(1)
-//     //       .toArray((err, result) => {
-//     //         if (err) return console.log(err);
-//     //         console.log(result);
-//     //       })
-//     //   );
-//     // });
-
-//     app.listen(port, () => {
-//       console.log("listening on http://localhost:" + port + "/");
-//     });
-// //   }
-// // );
-
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var favicon = require('serve-favicon')
+var path = require('path')
 var port = process.env.PORT || 3000;
-var data;
 const MongoClient = require("mongodb").MongoClient;
 var db;
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -71,11 +26,11 @@ io.on("connection", function(socket) {
       db.collection("data")
         .find()
         .sort({ _id: -1 })
-        .limit(1)
+        .limit(20)
         .toArray((err, result) => {
           if (err) return console.log(err);
           console.log(result);
-          io.emit("data", result[0]);
+          io.emit("data", result);
         });
 
       const stream = db.collection("data").watch();
@@ -84,7 +39,7 @@ io.on("connection", function(socket) {
           let result = [];
           result.push(change.fullDocument);
           console.log(result);
-          io.emit("data", result[0]);
+          io.emit("data", result);
         }
       });
     }
